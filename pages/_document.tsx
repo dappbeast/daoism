7a64d608f2cd7fbdc26265b30c8755f33313c4d1
earthtojake <jakehenryfitz@gmail.com>
2022-11-05 13:26:49 -0700

    pages

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Workpass</title>
+        <meta name="description" content="The proof of employment app" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,3 @@
-import Head from "next/head";
-
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Workpass</title>
-        <meta name="description" content="The proof of employment app" />
-      </Head>
-    </div>
-  );
+export default function Onboarding() {
+  return <p>Onboarding page</p>;
 }
