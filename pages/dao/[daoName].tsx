7b3c18b30d7b06c638c89aadd6e886dd6302c295
Isaac Kim <isaacmmikim@gmail.com>
2022-11-05 17:07:12 -0700

    skeleton dao page.

--- pages/dao/[daoName].tsx
+++ pages/dao/[daoName].tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 import useDAO from "../../hooks/useDAO";
+import { Heading, HStack, Text, VStack, Flex } from "@chakra-ui/react";
 
 export default function DAOPage() {
   const { query } = useRouter();
@@ -17,16 +18,30 @@ export default function DAOPage() {
   }
 
   return (
-    <div>
-      <h1>{dao.name}</h1>
-      <Image src={dao.logoUri} alt="Logo" width={120} height={120} />
-      <h2>Agreements</h2>
-      {dao.agreements.map((agreement) => (
-        <p key={agreement.id}>
-          ID #{agreement.id} - {agreement.role} - {dao.name} -{" "}
-          {agreement.recipient}
-        </p>
-      ))}
-    </div>
+    //<div>
+    //  <h1>{dao.name}</h1>
+    //  <Image src={dao.logoUri} alt="Logo" width={120} height={120} />
+    //  <h2>Agreements</h2>
+    //  {dao.agreements.map((agreement) => (
+    //    <Text color={"white"} key={agreement.id}>
+    //      ID #{agreement.id} - {agreement.role} - {dao.name} -{" "}
+    //      {agreement.recipient}
+    //    </Text>
+    //  ))}
+    //</div>
+    <VStack mt={20}>
+      <HStack>
+        {/*<Image src={dao.logoUri} alt="Logo" width={120} height={120} />*/}
+
+        <Heading as={"h1"} size={"xl"} color={"white"}>
+          {daoName}
+        </Heading>
+      </HStack>
+      <Flex>
+        <Heading as={"h3"} size={"md"} color={"whi"}>
+          Agreements
+        </Heading>
+      </Flex>
+    </VStack>
   );
 }
