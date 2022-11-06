8526c378b9445eb8049c679f3f4e0b880e8ca6f0
earthtojake <jakehenryfitz@gmail.com>
2022-11-06 01:31:27 -0800

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,24 @@ export default class MyDocument extends Document {
           <link href="/fonts/style.css" rel="stylesheet" />
           <link rel="preconnect" href="https://rsms.me/" />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body className="ph-no-capture">
           <Main />
