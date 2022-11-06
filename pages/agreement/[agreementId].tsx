2afbe024bc5557f7d667dcaab0b254b87f3c81b0
Isaac Kim <isaacmmikim@gmail.com>
2022-11-06 02:05:31 -0800

    Agreement details page finished.

--- pages/agreement/[agreementId].tsx
+++ pages/agreement/[agreementId].tsx
@@ -19,6 +19,11 @@ export default function AgreementPage() {
 
   const { data: agreement, isLoading } = useAgreement(agreementId);
 
+  const recipientPrefix =
+    agreement?.recipient.substring(0, 6) +
+    ".." +
+    agreement?.recipient.substring(agreement?.recipient.length - 4);
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
@@ -27,83 +32,86 @@ export default function AgreementPage() {
     return <p>Agreement does not exist</p>;
   }
 
+  console.log(agreement.startDate);
+
+  const startDate = new Date(
+    agreement.startDate * 1000 ?? 0
+  ).toLocaleDateString();
+  const endDate = new Date(agreement.endDate ?? 0).toLocaleDateString();
+
   console.log(agreement);
 
   return (
-    //<div>
-    //  <h1>Agreement {agreement.id}</h1>
-    //  <Text color="white">{JSON.stringify(agreement)}</Text>
-    //</div>
-
-    <VStack mt={30} mb={60} w={"1130px"} spacing={12} alignItems={"flex-start"}>
+    <VStack mt={30} mb={60} w={"1130px"} spacing={16} alignItems={"flex-start"}>
       <Heading as={"h1"} color={"white"} fontSize={60}>
         Agreement Details
       </Heading>
       <Box>
         <Heading as={"h1"} color={"white"}>
-          {agreementId}
+          ID {agreementId}
         </Heading>
 
         <VStack
           w={"1130px"}
+          mt={8}
           mb={30}
           divider={<StackDivider borderColor="gray.200" />}
-          spacing={20}
+          spacing={12}
         >
           <HStack w={"100%"} spacing={30}>
             <VStack w={"50%"} alignItems={"flex-start"}>
               <Heading as={"h3"} color={"white"} fontSize={26}>
                 Contributor Wallet Address
               </Heading>
-              <Text color={"grey"}>{agreement.recipient}</Text>
+              <Text color={"white"}>{recipientPrefix}</Text>
             </VStack>
             <VStack w={"50%"} alignItems={"flex-start"}>
               <Heading as={"h1"} color={"white"} fontSize={26}>
                 Contributor Role
               </Heading>
-              <Text color={"grey"}>{agreement.role}</Text>
+              <Text color={"white"}>{agreement.role}</Text>
             </VStack>
           </HStack>
 
-          <VStack w={"100%"} alignItems={"flex-start"} spacing={6}>
+          <VStack w={"100%"} alignItems={"flex-start"} spacing={3}>
             <Heading as={"h3"} color={"white"} fontSize={26}>
               Salary
             </Heading>
 
             <HStack w={"100%"}>
-              <VStack w={"50%"} alignItems={"flex-start"}>
+              <VStack w={"50%"} alignItems={"flex-start"} spacing={1}>
                 <Text color={"grey"} fontSize={16} fontWeight={700}>
                   Amount
                 </Text>
-                <Text color={"grey"}>{agreement.salary}</Text>
+                <Text color={"white"}>{agreement.salary}</Text>
               </VStack>
-              <VStack w={"50%"} alignItems={"flex-start"}>
+              <VStack w={"50%"} alignItems={"flex-start"} spacing={1}>
                 <Text color={"grey"} fontSize={16} fontWeight={700}>
                   Currency
                 </Text>
-                <Text color={"grey"}>USDC</Text>
+                <Text color={"white"}>USDC</Text>
               </VStack>
             </HStack>
           </VStack>
 
-          <VStack w={"100%"} alignItems={"flex-start"} spacing={6}>
+          <VStack w={"100%"} alignItems={"flex-start"} spacing={3}>
             <Heading as={"h3"} color={"white"} fontSize={26}>
               Duration
             </Heading>
 
             <HStack w={"100%"}>
-              <VStack w={"50%"} alignItems={"flex-start"}>
+              <VStack w={"50%"} alignItems={"flex-start"} spacing={1}>
                 <Text color={"grey"} fontSize={16} fontWeight={700}>
                   Start Date
                 </Text>
-                <Text color={"grey"}>{agreement.startDate}</Text>
+                <Text color={"white"}>{startDate}</Text>
               </VStack>
-              <VStack w={"50%"} alignItems={"flex-start"}>
+              <VStack w={"50%"} alignItems={"flex-start"} spacing={1}>
                 <Text color={"grey"} fontSize={16} fontWeight={700}>
                   End Date
                 </Text>
-                <Text color={"grey"}>
-                  {agreement.endDate === null ? "-" : agreement.endDate}
+                <Text color={"white"}>
+                  {agreement.endDate === null ? "-" : endDate}
                 </Text>
               </VStack>
             </HStack>
